77216f3ab695865dd414c321584c92aef9202b9f
allanos662 <99630862+allanos662@users.noreply.github.com>
2022-03-01 10:35:29 -0800

    Update vdparduino.py

--- fal_lab_drivers/vdparduino.py
+++ fal_lab_drivers/vdparduino.py
@@ -5,6 +5,7 @@ import time
 class ard(serial.Serial):
     def __init__(self, arduino_address, tout):
         serial.Serial.__init__(self, arduino_address, timeout = tout)
+    return "connected to vdp switch
 
     def switch(self, vdp_switch):
         self.write(bytes(str(vdp_switch), 'utf-8'))
