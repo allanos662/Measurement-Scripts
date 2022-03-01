7b5bab4f31d4ab052e255792729b876e969c556f
allanos662 <99630862+allanos662@users.noreply.github.com>
2022-03-01 09:34:51 -0800

    Add files via upload

--- PPMS-vdp-SR830-RH.py
+++ PPMS-vdp-SR830-RH.py
@@ -10,7 +10,7 @@ import yaml
 import datetime as dt
 
 #connect to vdp arduino switch
-with open('temp_sweep.yaml', 'r') as f:
+with open('field_sweep.yaml', 'r') as f:
     params = yaml.load(f, Loader=yaml.FullLoader)
 
 vanderpaw = ard('COM4', 0.1)
@@ -30,7 +30,7 @@ for key in params['LIA']: #record LIA parameters in datafile
 data_file.write('T_PPMS(K)\t H_PPMS(Oe)\tVDP_config\tLIA_X(V)\tLIA_Y(V)\tLIA_PHASE\n')
 
 #connect to dynacool, lock in, set vdp configs
-dynacool = DynaCool('dynacool', address="TCPIP0::127.0.0.1::5000::SOCKET")
+dynacool = QdInstrument('Dynacool')
 
 sr = SR830('lockin', 'GPIB0::'+str(params['LIA']['ADDRESS'])+'::INSTR')
 setup_lia(sr, params['LIA'])
@@ -48,33 +48,31 @@ while(1):
     if go == 'n':
         quit()    
 
-dynacool.temperature_rate(params['QUICK_RAMP_TEMP'])
-dynacool.temperature_setpoint(params['T_START'])
-
-dynacool.field_rate(params['QUICK_RAMP_FIELD'])
-dynacool.field_ramp(params['H_START'])
+dynacool.setTemperature(params['T_START'], params['QUICK_RAMP_TEMP'])
+dynacool.setField(params['H_START'], params['QUICK_RAMP_FIELD'])
 print("ramping to starting temperature and field setpoints...")
 
-while dynacool.temperature_state() != 'settled' or dynacool.magnet_state() != 'settled'
-    continue
+waitForField()
+waitForTemperature()
 
 print(f"holding for additional settling time: {params['SETTLE']} mins")
 time.sleep(params['SETTLE'])
 
 #Measurment Loop
 #begin ramp command
-dynacool.field_rate(params['H_RAMP_MEASURE'])
-dynacool.field_ramp(params['H_FINAL'])
 
-#print(f'ramping to {params[T_FINAL]} at {params[SWEEP_RATE]}K/min')
-while dynacool.magnet_state() == 'chasing':
+dynacool.setField(params['H_FINAL'], params['H_RAMP_MEASURE'])
+while(1):
     for config in active_switches:
         vanderpauw.switch(config) #changes the vanderpaw config 
         time.sleep(params['DELAY']) #waits for set delay before recording
-        temp = dynacool.temperature() #gets Temp
-        H = dynacool.field_measured() #gets field
+        temp = dynacool.getTemperature() #gets Temp
+        H = dynacool.getField() #gets field
 
         timenow = dt.datetime.now() #gets time
         values = sr.snap('x', 'y', 'phase') #measured values from LIA
 
         data_file.write(f'{timenow.hour}:{timenow.minute}:{timenow.second}\t{temp}\t{H}\t{config}\t{values[0]}\t{values[1]}\t{values[2]}\n')
+        if H == params['H_final']:
+            break
+
